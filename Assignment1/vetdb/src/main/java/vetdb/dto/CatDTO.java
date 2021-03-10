2d990e98c46fa82a382a61f12f7037fffae4d8e7
Jörg Oertel <tyskerjoerg@gmail.com>
2021-03-10 19:19:09 +0100

    added views

---
+++ Assignment1/vetdb/src/main/java/vetdb/dto/CatDTO.java
@@ -0,0 +1,15 @@
+package vetdb;
+
+import vetdb.entities.CatsEntity;
+import vetdb.entities.PetsEntity;
+
+public class CatDTO {
+
+    private CatsEntity cats;
+    private PetsEntity pets;
+    private int id, age, vet_cpr, life_count;
+    private String name;
+
+
+
+}
---
+++ Assignment1/vetdb/src/main/java/vetdb/facade/Facade.java
@@ -0,0 +1,2 @@
+package vetdb.facade;public class Facade {
+}
